51f3119921c0f9b6fc2a728ce66267ab6f2b2980
Ryan Williams <bigwookie3000@gmail.com>
2021-01-13 18:27:00 -0500

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,13 @@
+/*
+Program Name: HelloWorld.java
+Purpose: Print text to screen
+Coder: Ryan Williams
+Date: January 2021
+*/
+Public class HelloWorld
+{
+	Public static void main(String[]args)//the front door
+	{
+		System.out.println("Hello World!);
+	}//End main
+}//End class
